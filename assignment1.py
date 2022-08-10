99c7109e51afa1533fe6e532ee15cd114590565b
mhkagg <109355634+mhkagg@users.noreply.github.com>
2022-08-10 14:23:41 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,9 +1,9 @@
 #write a Python program which accepts the radius of a circle from the user and computes area.
-# from cmath import pi
+from cmath import pi
 
-# radius= float(input("Input the radius of the circle:"))
-# area= pi*(radius**2)
-# print(area)
+radius= float(input("Input the radius of the circle:"))
+area= pi*(radius**2)
+print(area)
 
 #write a Python program to accept a filename from the user and print the extension of that.
 filename= input("Input the filename:")
