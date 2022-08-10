3ed42c664ba597d3b2a8c87ccd10f3542de1d226
mhkagg <109355634+mhkagg@users.noreply.github.com>
2022-08-10 14:23:04 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+#write a Python program which accepts the radius of a circle from the user and computes area.
+# from cmath import pi
+
+# radius= float(input("Input the radius of the circle:"))
+# area= pi*(radius**2)
+# print(area)
+
+#write a Python program to accept a filename from the user and print the extension of that.
+filename= input("Input the filename:")
+extn=filename.split(".")
+print(filename)
+print ("The extension of the file is : ", extn[-1])
